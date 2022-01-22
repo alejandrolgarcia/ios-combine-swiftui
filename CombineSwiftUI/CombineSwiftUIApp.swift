25d36bda729b6f0d57c215553d5b0c59f26eae15
Luis Garcia <lalejandro.1988@gmail.com>
2022-01-22 12:33:34 -0800

    Initial Commit

---
+++ CombineSwiftUI/CombineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineSwiftUIApp.swift
+//  CombineSwiftUI
+//
+//  Created by Luis Garcia on 22/01/22.
+//
+
+import SwiftUI
+
+@main
+struct CombineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
